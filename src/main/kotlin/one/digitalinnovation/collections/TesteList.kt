838cce2e690a1bc9f92b0eaf6dbb3f666f521130
Thaís Felício de Paula Silva <thsfelicio@gmail.com>
2021-08-29 17:45:33 -0300

    usando sortedBy

--- src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
+++ src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
@@ -1,8 +1,8 @@
 package one.digitalinnovation.collections
 
 fun main () {
-    val joao = funcionario ( nome = "Joao", salario = 1000.0)
-    val pedro = funcionario ( nome = "Pedro", salario = 2000.0)
+    val joao = funcionario ( nome = "Joao", salario = 2000.0)
+    val pedro = funcionario ( nome = "Pedro", salario = 1500.0)
     val maria = funcionario ( nome = "Maria", salario = 4000.0)
 
 
@@ -10,6 +10,11 @@ fun main () {
 
     println("---------------------------------")
     println(funcionarios.find { it.nome == "Maria" })
+
+    println("---------------------------------")
+    funcionarios.sortedBy { it.salario }
+                .forEach { println(it) }
+
     funcionarios.forEach { println(it) }
 }  data class funcionario(val nome: String, val salario: Double)
 {
