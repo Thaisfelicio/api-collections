d06b4e4a882916036112d7dd7a002eb8f891d5f6
Thaís Felício de Paula Silva <thsfelicio@gmail.com>
2021-08-29 17:56:32 -0300

    usando groupBy

--- src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
+++ src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
@@ -1,9 +1,9 @@
 package one.digitalinnovation.collections
 
 fun main () {
-    val joao = funcionario ( nome = "Joao", salario = 2000.0)
-    val pedro = funcionario ( nome = "Pedro", salario = 1500.0)
-    val maria = funcionario ( nome = "Maria", salario = 4000.0)
+    val joao = funcionario ( nome = "Joao", salario = 2000.0, tipocontratacao = "CLT")
+    val pedro = funcionario ( nome = "Pedro", salario = 1500.0, tipocontratacao = "PJ")
+    val maria = funcionario ( nome = "Maria", salario = 4000.0, tipocontratacao = "CLT")
 
 
     val funcionarios = listOf(joao, pedro, maria)
@@ -12,17 +12,26 @@ fun main () {
     println(funcionarios.find { it.nome == "Maria" })
 
     println("---------------------------------")
-    funcionarios.sortedBy { it.salario }
-                .forEach { println(it) }
+    funcionarios
+        .sortedBy { it.salario }
+        .forEach { println(it) }
+
+    println("---------------------------------")
+    funcionarios
+        .groupBy { it.tipocontratacao }
+        .forEach { println(it) }
 
     funcionarios.forEach { println(it) }
-}  data class funcionario(val nome: String, val salario: Double)
+
+}   data class funcionario(val nome: String,
+                          val salario: Double,
+                          val tipocontratacao: String)
 {
         override fun toString(): String =
             """""
         Nome:    $nome
         Salário: $salario
-        """"
+        """""
            
         
         
