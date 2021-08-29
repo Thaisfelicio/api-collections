339c8bea5f95177d75ff713b90cf5ce8dfd9698d
Thaís Felício de Paula Silva <thsfelicio@gmail.com>
2021-08-29 15:59:34 -0300

    testando operações

---
+++ src/main/kotlin/one/digitalinnovation/collections/TesteOperacoes.kt
@@ -0,0 +1,9 @@
+package one.digitalinnovation.collections
+
+fun main () {
+    val salarios = doubleArrayOf(1000.0, 2250.0, 4000.0)
+     for (salario in salarios) {
+         println(salario)
+     }
+     println("¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨")
+}
